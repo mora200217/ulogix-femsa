0c495dcebbf097fc5ec1c12b1cc0efbd33070259
Andrés Morales <amoralesma@unal.edu.co>
2026-03-22 19:45:08 -0500

    [Update] base webpage from astro

---
+++ astro.config.mjs
@@ -0,0 +1,59 @@
+import { defineConfig, fontProviders } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'InterLocal',
+      cssVariable: '--font-inter',
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: 'normal',
+            src: ['./src/assets/fonts/Inter-Regular.woff2'],
+          },
+          {
+            weight: 600,
+            style: 'normal',
+            src: ['./src/assets/fonts/Inter-SemiBold.woff2'],
+          },
+          {
+            weight: 700,
+            style: 'normal',
+            src: ['./src/assets/fonts/Inter-Bold.woff2'],
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: 'InterLocalDisplay',
+      cssVariable: '--font-inter-display',
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: 'normal',
+            src: ['./src/assets/fonts/InterDisplay-Regular.woff2'],
+          },
+          {
+            weight: 500,
+            style: 'normal',
+            src: ['./src/assets/fonts/InterDisplay-Medium.woff2'],
+          },
+          {
+            weight: 600,
+            style: 'normal',
+            src: ['./src/assets/fonts/InterDisplay-SemiBold.woff2'],
+          },
+        ],
+      },
+    },
+  ],
+})
